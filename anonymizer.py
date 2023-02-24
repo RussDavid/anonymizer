366147494abee32d99c2187e72f1e5a591f42418
Russ <russ.nz@outlook.com>
2023-02-25 01:14:44 +1300

    Added functionality to load user replacement lists

--- anonymizer.py
+++ anonymizer.py
@@ -8,46 +8,47 @@ from random import randint, choice
 import pandas as pd
 import faker
 
-class AnonymizerWrapper():
+class AnonymizerConfigurator():
     NAMED_GROUP_PATTERN = re.compile(r'\(\?P<\w*>.+\)')
     
-    def __init__(self, _field_pattern_mapping, _custom_replacement_values) -> None:
+    def __init__(self, _field_pattern_mapping, 
+                 _custom_replacement_values, 
+                 _no_of_values_to_generate=10000,
+                 _faker_seed=0) -> pd.Series:
         self.field_pattern_mapping = _field_pattern_mapping
-        
         self.custom_replacement_values = _custom_replacement_values
+        self.no_of_values_to_generate = _no_of_values_to_generate
+        self.faker_seed = _faker_seed
         self.__validate_regex_pattern()
-       
         self.__build_replacement_value_lists()
-        self.replacement_values = {
-            'fnames': self.first_name_values,
-            'lnames': self.last_name_values,
-            'emails': self.email_address_values,
-            'house_nums': self.house_no_values,
-            'streets': self.street_values,
-            'cities': self.city_values
-        }
-        if _custom_replacement_values is not None:
-            for regex_group_key in _custom_replacement_values:
-                self.replacement_values[regex_group_key] = _custom_replacement_values[regex_group_key]
-            
+
     def __build_replacement_value_lists(self):
-        self.first_name_values = []
-        self.last_name_values = []
-        self.email_address_values = []
-        self.house_no_values = []
-        self.street_values = []
-        self.city_values = []
+        self.first_name_values, self.last_name_values = [], []
+        self.house_no_values, self.street_values = [], []
+        self.city_values, self.email_address_values = [], []
         
         fake = faker.Faker()
-        faker.Faker.seed(0)
-        for _ in range(25):
+        faker.Faker.seed(self.faker_seed)
+        for _ in range(self.no_of_values_to_generate):
             self.first_name_values.append(fake.first_name())
             self.last_name_values.append(fake.last_name())
             self.email_address_values.append(fake.email())
             self.house_no_values.append(fake.building_number())
             self.street_values.append(fake.street_address())
             self.city_values.append(fake.city())
+
+        self.replacement_values = {
+            'fname': self.first_name_values,
+            'lname': self.last_name_values,
+            'email': self.email_address_values,
+            'house_no': self.house_no_values,
+            'street_name': self.street_values,
+            'city': self.city_values
+        }
         
+        if self.custom_replacement_values is not None:
+            for regex_group_key in self.custom_replacement_values:
+                self.replacement_values[regex_group_key] = self.custom_replacement_values[regex_group_key]
             
     def __validate_regex_pattern(self):
         for pattern in self.field_pattern_mapping.values():
@@ -55,22 +56,19 @@ class AnonymizerWrapper():
                 raise re.error(msg=f'The Regular Expression: {pattern} is invalid, it does not contain a named group')
             
     def anonymize_record(self, _row):
-        try:
-            anon = Anonymizer(row=_row,
-                              replacement_dict=self.replacement_values,
-                              field_pattern_dict=self.field_pattern_mapping)
-            
-            return anon.randomize_fields()
-        except Exception as err:
-            print(err)
-            print(traceback.format_exc())
+        anon = RecordAnonymizer(row=_row,
+                            replacement_dict=self.replacement_values,
+                            field_pattern_dict=self.field_pattern_mapping)
+        return anon.randomize_fields()
         
         
-class Anonymizer():
+class RecordAnonymizer():
     LETTERS_LOWER = string.ascii_lowercase
     LETTERS_UPPER = string.ascii_uppercase
     
     def __init__(self, row, replacement_dict, field_pattern_dict) -> None:
+        # TODO: move replacement_dict and field_pattern_dict init into
+        # a class method
         self.row = row
         self.replacement_dict = replacement_dict
         self.field_pattern_dict = field_pattern_dict
@@ -85,43 +83,40 @@ class Anonymizer():
             'chars': self.__randomize_chars,
             'fname': {
                 'function': self.__get_anonymized_value,
-                'replacement_list': self.replacement_dict['fnames'],
+                'replacement_list': self.replacement_dict['fname'],
                 'current_value': None
             },
             'lname': {
                 'function': self.__get_anonymized_value,
-                'replacement_list': self.replacement_dict['lnames'],
+                'replacement_list': self.replacement_dict['lname'],
                 'current_value': None
             },
             'email': {
                 'function': self.__get_anonymized_value,
-                'replacement_list': self.replacement_dict['emails'],
+                'replacement_list': self.replacement_dict['email'],
                 'current_value': None
             },
             'street_name': {
                 'function': self.__get_anonymized_value,
-                'replacement_list': self.replacement_dict['streets'],
+                'replacement_list': self.replacement_dict['street_name'],
                 'current_value': None
             },
             'city': {
                 'function': self.__get_anonymized_value,
-                'replacement_list': self.replacement_dict['cities'],
+                'replacement_list': self.replacement_dict['city'],
                 'current_value': None
             },
             'post_code': self.__get_random_postcode,
             'phone': self.__get_random_nz_phone
         }
-        # self.function_map = {
-        #     'digits': self.__randomize_digits,
-        #     'chars': self.__randomize_chars,
-        #     'fname': self.__get_random_first_name,
-        #     'lname': self.__get_random_last_name,
-        #     'email': self.__get_random_email,
-        #     'street_name': self.__get_random_street,
-        #     'city': self.__get_random_city,
-        #     'post_code': self.__get_random_postcode,
-        #     'phone': self.__get_random_nz_phone
-        # }
+        
+        if self.replacement_dict is not None:
+            for replacement_key in self.replacement_dict:
+                self.function_map[replacement_key] = {
+                    'function': self.__get_anonymized_value,
+                    'replacement_list': self.replacement_dict[replacement_key],
+                    'current_value': None
+                }
     
     def __get_anonymized_value(self, source_list):
         return source_list[randint(0, len(source_list) - 1)]
@@ -151,33 +146,40 @@ class Anonymizer():
             self.phone = '642' + str(randint(10000000, 99999999))
         return self.phone         
     
+    # Loop over each field in field to pattern mapping 
+    # Get the column value from the Pandas series
+    # Check if the column value contains a match to the regex pattern
     def randomize_fields(self):
         for field, pattern in self.field_pattern_dict.items():
             match = re.search(pattern, self.row[field])
             if match:
-                match_dict = match.groupdict()
-                
-                for key in match_dict:
-                    if match_dict[key] in [None, '']:
+                match_groups = match.groupdict()
+                for match_group_name, match_group_value in match_groups.items():
+                    # TODO: add option to replace empty values?
+                    if match_group_value in [None, '', 'nan']:
                         continue
                     
-                    randomizer_function = self.function_map[key]
-                    if type(randomizer_function) is dict:
-                        if randomizer_function['current_value'] is None:
-                            randomized_value = randomizer_function['function'](randomizer_function['replacement_list'])
-                            randomizer_function['current_value'] = randomized_value
+                    # From the function map get a function or dictionary containing
+                    # the relevant function to anonoymize this regex group value
+                    # Randomized values that are used across multiple columns are stored 
+                    match_group_randomizer = self.function_map[match_group_name]
+                    if type(match_group_randomizer) is dict:
+                        if match_group_randomizer['current_value'] is None:
+                            randomizer_function = match_group_randomizer['function']
+                            random_replacement_values = match_group_randomizer['replacement_list']
+                            randomized_value = randomizer_function(random_replacement_values)
+                            match_group_randomizer['current_value'] = randomized_value
                         else:
-                            randomized_value = randomizer_function['current_value']
-                    else:
-                        print(match_dict[key], key)
-                        randomized_value = randomizer_function(match_dict[key])
+                            randomized_value = match_group_randomizer['current_value']
+                    else: # This type of match group does not persist values across columns
+                        randomized_value = match_group_randomizer(match_group_value)
 
-                    self.row[field] = self.row[field].replace(match_dict[key], randomized_value)
+                    self.row[field] = self.row[field].replace(match_group_value, randomized_value)
             else:
                 pass
                 # print(f'No match found for expression: {pattern} on field {field} for value {self.row[field]}')
-                
         return self.row
+
     
 if __name__ == '__main__':
     pattern_file = pathlib.Path('patterns.ini')
@@ -197,6 +199,17 @@ if __name__ == '__main__':
                         default=data_file,
                         help='The path to the file that contains the data the anonymized. '\
                             'Default Path: .\data.csv')
+    parser.add_argument('--generate-values', '-gv',
+                        type=int,
+                        default=10000,
+                        help='The number of random replacement values each replacement list '\
+                            'should contain.  Default: 10,000')
+    parser.add_argument('--faker-seed', '-fs',
+                        type=int,
+                        default=0,
+                        help='The seed value that Faker uses. Changing this will change which '\
+                            'values are selected to be populated into the replacement lists. '\
+                            'Default: 0')
     args = parser.parse_args()
     
     if str(args.pattern_file_path) != str(pattern_file):
@@ -216,7 +229,7 @@ if __name__ == '__main__':
         
         # Combine the two sections in the configuration file into one dictionary
         # of the structure: field_name: regular_expression_string
-        # 'first_name': '(?<fname>/w+)
+        # e.g. 'first_name': '(?<fname>/w+)
         # Get the name of the associated pattern for this field contained in
         # the [Regex_Patterns] section
         field_to_pattern = {}
@@ -224,24 +237,56 @@ if __name__ == '__main__':
             pattern_name = config['Field_Mapping'][key]
             regex_pattern = config['Regex_Patterns'][pattern_name]
             field_to_pattern[key] = regex_pattern
-            
-        print(field_to_pattern)
+        
+        # Get the user created lists of replacement values from the config file
+        # This is read as a string which then needs to be converted to a list
+        user_replacement_values = {}
+        path_regex = re.compile(pattern=r'^(((\w+:*|\d+)?(\\|\/))*(\w+|\d+))+\.(csv|txt)\s*$', 
+                                flags=re.IGNORECASE)
+        if 'Replacement_Values' in config:
+            for replacement_key in config['Replacement_Values']:
+                replacement_values = config['Replacement_Values'][replacement_key]
+                # If the replacement value is a path instead of a list
+                # then the replacement values must be loaded from that file
+                if re.match(path_regex, replacement_values):
+                    replacement_value_file = pathlib.Path(replacement_values)
+                    replacement_df = pd.read_csv(replacement_value_file, header=None)
+                    file_values = replacement_df.iloc[0].values
+                    user_replacement_values[replacement_key] = file_values
+                else:
+                    replacement_values = replacement_values.removeprefix('[').removesuffix(']')
+                    replacement_values = replacement_values.split(', ')
+                    user_replacement_values[replacement_key] = replacement_values
+
         df = pd.read_csv(data_file)
         df = df.astype(str)
         
         print('Finished initial configuration')
-    except configparser.NoSectionError as e:
-        print(f'{e}\nPattern config file must both [Regex_Patterns] and [Field_Mapping] sections')
-        print(traceback.format_exc())
-    except Exception as e:
-        print(e)
-        print(traceback.format_exc())
-    
-    try:
-        anonymizer = AnonymizerWrapper(field_to_pattern, None)
+        print('Creating Anonymizer object')
         
+        anonymizer = AnonymizerConfigurator(field_to_pattern, user_replacement_values,
+                                            args.generate_values, args.faker_seed)
         df_anon = df.apply(lambda x: anonymizer.anonymize_record(x), axis=1)
         print(df_anon)
-    except Exception as e:
-        print('Exception occured')
+    except configparser.NoSectionError as error:
         print(traceback.format_exc())
+        print(f'{error}\nPattern config file must both [Regex_Patterns] and [Field_Mapping] sections')
+    except re.error as error:
+        print(traceback.format_exc())
+        print(error)
+    except FileNotFoundError as error:
+        print(traceback.format_exc())
+        print(f'The file {error.filename} could not be found.\n'
+              'Check that the file exists and the path is valid.')
+    except KeyError as error:
+        print(traceback.format_exc())
+        print(f'An error occured, KeyError: {error}')
+        print('This error is most likely caused due to incorrect mapping between\n'
+              'the [Field_Mapping] in the config file and the columns present\n'
+              'in the data file. The names of the fields defined in the [Field_Mapping]\n'
+              'must match the names of the columns in the data file.\n'
+              'Field and column names are case-sensitive.')
+    except Exception as e:
+        print(traceback.format_exc())
+        print(e)
+        print('Some other exception occurred.')
